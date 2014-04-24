8460529dc1e2bd16e328e16954c47d053a4a8f48
Cary Lee <carylee@gmail.com>
2014-04-23 23:20:57 -0700

    Added a POST endpoint for barks

--- src/barktracker_server/routes/home.clj
+++ src/barktracker_server/routes/home.clj
@@ -1,9 +1,14 @@
 (ns barktracker-server.routes.home
   (:require [compojure.core :refer :all]
+            [barktracker-server.models.bark :as bark]
             [barktracker-server.views.layout :as layout]))
 
 (defn home []
   (layout/common [:h1 "Hello World!"]))
 
+(defn add-bark [pad length start]
+  (bark/add! pad length start))
+
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (POST "/barks" [pad length start] (add-bark pad length start)))
